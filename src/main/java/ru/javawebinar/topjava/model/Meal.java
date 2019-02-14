68b67d569c514d11b5a548b5e871e33890009bc9
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-02-14 21:07:58 +0300

    Added userId field into Meal and MealTo

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -12,15 +12,22 @@ public class Meal extends AbstractBaseEntity {
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    private final int userId;
+
+    public Meal(Integer userId, LocalDateTime dateTime, String description, int calories) {
+        this(null, userId, dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
         super(id);
+        this.userId = userId;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public LocalDateTime getDateTime() {
--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class UsersUtil {
     public static final List<User> USERS = Arrays.asList(
-            new User(null, "Admin Name", "admin@mail.ru", "admin_password", Role.ROLE_ADMIN),
+            new User(null, "Admin Name", "admin@mail.ru", "admin_password", Role.ROLE_ADMIN, Role.ROLE_USER),
             new User(null, "User Name", "user@mail.ru", "user_password", Role.ROLE_USER)
     );
 }
