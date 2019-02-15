3238ff1b58b1fa7cbb393e50813d0f08c60caac8
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-02-15 10:34:24 +0300

    MealServiceImpl/MealService implementation

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,24 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface MealService {
+
+    Meal create(Meal meal);
+
+    void delete(int id, int userId) throws NotFoundException;
+
+    Meal get(int id, int userId) throws NotFoundException;
+
+    void update(Meal meal) throws NotFoundException;
+
+    List<Meal> getAll(int userId);
+
+    List<Meal> getAll(int userId, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime);
+
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,51 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    @Override
+    public void delete(int id, int userId) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    @Override
+    public void update(Meal meal) throws NotFoundException {
+        checkNotFoundWithId(repository.save(meal), meal.getId());
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return new ArrayList<>(repository.getAll(userId));
+    }
+
+    @Override
+    public List<Meal> getAll(int userId, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+        return new ArrayList<>(repository.getAll(userId, startDate, endDate, startTime, endTime));
+    }
 }
