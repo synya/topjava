c207759dc852bc9a997a7a79bef047bb8b82c90f
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-03-01 13:55:27 +0300

    Added @Rule to MealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,15 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,6 +34,41 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    private static StringBuilder stringBuilder = new StringBuilder("\n");
+
+    @Rule
+    public final TestRule testRule = new TestWatcher() {
+        private long startTime;
+
+        @Override
+        protected void starting(Description description) {
+            super.starting(description);
+            startTime = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void finished(Description description) {
+            long executionTime = System.currentTimeMillis() - startTime;
+            String executionTimeString = "execution time: " + executionTime + " ms.";
+            log.info(executionTimeString);
+            stringBuilder.append(description.getDisplayName());
+            stringBuilder.append(": ");
+            stringBuilder.append(executionTimeString);
+            stringBuilder.append("\n");
+            super.finished(description);
+        }
+    };
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println(stringBuilder.toString());
+    }
+
     static {
         SLF4JBridgeHandler.install();
     }
@@ -39,8 +82,10 @@ public class MealServiceTest {
         assertMatch(service.getAll(USER_ID), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void deleteNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
+        expectedException.expectMessage("Not found entity with id=" + MEAL1_ID);
         service.delete(MEAL1_ID, 1);
     }
 
@@ -59,8 +104,10 @@ public class MealServiceTest {
         assertMatch(actual, ADMIN_MEAL1);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void getNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
+        expectedException.expectMessage("Not found entity with id=" + MEAL1_ID);
         service.get(MEAL1_ID, ADMIN_ID);
     }
 
@@ -71,8 +118,10 @@ public class MealServiceTest {
         assertMatch(service.get(MEAL1_ID, USER_ID), updated);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void updateNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
+        expectedException.expectMessage("Not found entity with id=" + MEAL1.getId());
         service.update(MEAL1, ADMIN_ID);
     }
 
