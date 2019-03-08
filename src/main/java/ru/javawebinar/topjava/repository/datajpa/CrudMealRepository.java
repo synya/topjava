a50a98d9c8c5d4b40bc44434acb1afbf47d56efc
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-03-08 18:12:32 +0300

    DataJpaMealRepositoryImpl implementation

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,26 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Transactional
+    @Modifying
+    int deleteByIdAndUserId(int id, int userId);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    Meal getByIdAndUserId(Integer id, Integer userId);
+
+    List<Meal> getAllByUserIdOrderByDateTimeDesc(int userId);
+
+    List<Meal> getAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(int userId, LocalDateTime startDate, LocalDateTime endDate);
 }
