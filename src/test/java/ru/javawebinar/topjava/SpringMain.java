bdc2359b127bd63a81ebeb248791302b276859da
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-03-09 18:50:46 +0300

    added runtime profile configuration to the MealServlet and SpringMain

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
@@ -16,19 +16,21 @@ import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
-        // java 7 automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
+        GenericXmlApplicationContext springContext = new GenericXmlApplicationContext();
+        ConfigurableEnvironment springContextEnvironment = springContext.getEnvironment();
+        springContextEnvironment.setActiveProfiles("hsqldb", "jdbc");
+        springContext.load("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.refresh();
+        System.out.println("Bean definition names: " + Arrays.toString(springContext.getBeanDefinitionNames()));
+        AdminRestController adminUserController = springContext.getBean(AdminRestController.class);
+        adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+        System.out.println();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
-        }
+        MealRestController mealController = springContext.getBean(MealRestController.class);
+        List<MealTo> filteredMealsWithExcess =
+                mealController.getBetween(
+                        LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                        LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+        filteredMealsWithExcess.forEach(System.out::println);
     }
 }
