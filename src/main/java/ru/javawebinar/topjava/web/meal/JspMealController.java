c4be1f85b478f29983b3daa810167846b1ca0fcd
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-03-18 19:47:01 +0300

    removed "action" parameter from jsp, using path for action resolving and PathVariable annotation to get parameter from path. Fixed relative path issue when using path for action resolving.

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -3,10 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
@@ -18,23 +15,22 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 @Controller
 @RequestMapping(value = "/meals")
 public class JspMealController extends AbstractMealController {
-    @GetMapping(params = {"action=delete", "id"})
-    public String delete(@RequestParam(value = "id") Integer id) {
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
         super.delete(id);
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
-    @GetMapping(params = "action=create")
+    @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("meal",
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
         return "mealForm";
     }
 
-    @GetMapping(params = {"action=update", "id"})
-    public String update(@RequestParam(value = "id") Integer id, Model model) {
-        model.addAttribute("meal",
-                super.get(id));
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("meal", super.get(id));
         return "mealForm";
     }
 
@@ -44,7 +40,7 @@ public class JspMealController extends AbstractMealController {
         return "meals";
     }
 
-    @PostMapping(params = "action=filter")
+    @PostMapping("/filter")
     public String getBetween(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate,
                              @RequestParam("startTime") String startTime, @RequestParam("endTime") String endTime,
                              Model model) {
@@ -62,6 +58,6 @@ public class JspMealController extends AbstractMealController {
         } else {
             super.update(meal, Integer.valueOf(id));
         }
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -8,7 +8,7 @@
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <section>
     <h3>Meals</h3>
-    <form method="post" action="meals?action=filter">
+    <form method="post" action="meals/filter">
         <dl>
             <dt>From Date:</dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -28,7 +28,7 @@
         <button type="submit">Filter</button>
     </form>
     <hr/>
-    <a href="meals?action=create">Add Meal</a>
+    <a href="meals/create">Add Meal</a>
     <hr/>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -51,8 +51,8 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}">Update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
+                <td><a href="meals/update/${meal.id}">Update</a></td>
+                <td><a href="meals/delete/${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
